dfdf293e9a10c15114e9e1d3edd311a09f242edf
Kriti Kalpa <krtklpsh@gmail.com>
2020-10-05 21:51:08 +0530

    [Kriti] Add. Second mandatory part of email

--- EmailValidation/Program.cs
+++ EmailValidation/Program.cs
@@ -9,7 +9,7 @@ namespace PincodeRegex
         {
             Console.WriteLine("Enter the Email");
             string pinCode = Console.ReadLine();
-            Regex reEmail = new Regex(@"^([a-z0-9][a-z0-9.+_-]*)$");
+            Regex reEmail = new Regex(@"^[a-z0-9][a-z0-9.+_-]*@([a-z0-9]([a-z0-9-]*[a-z]))$");
             if (reEmail.IsMatch(pinCode.ToString()))
             {
                 Console.WriteLine("Valid Email");
